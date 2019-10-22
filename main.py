7461ebcc609156288543b26e2a8300e44d9b77b1
LVillaflores <lorenzo@tanda.co>
2019-10-22 08:37:52 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from Timer import Timer
+import time
 
-timer = Timer()
-
-timer.print_time()
-
-timer.print_new_time()
-
-print(timer.get_time())
+t = int(input("Countdown from: "))
+while t >= 0:
+	mins, secs = divmod(t, 60)
+	timeformat = '{:02d}:{:02d}'.format(mins, secs)
+	time.sleep(1)
+	print("Time Remaining: "+ timeformat)
+	t -= 1
+print("Kaboom")
